fcdaa636da88092758144f190db83b579fdbf921
jackymadx <jacky.chan@madxstudio.com>
2015-04-22 11:17:34 +0800

    Network Lib

---
+++ NetworkLib/NetworkLib.h
@@ -0,0 +1,13 @@
+//
+//  NetworkLib.h
+//  NetworkLib
+//
+//  Created by Jacky Chan on 4/21/15.
+//  Copyright (c) 2015 Jacky Chan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkLib : NSObject
+
+@end
---
+++ NetworkLib/NetworkLib.m
@@ -0,0 +1,13 @@
+//
+//  NetworkLib.m
+//  NetworkLib
+//
+//  Created by Jacky Chan on 4/21/15.
+//  Copyright (c) 2015 Jacky Chan. All rights reserved.
+//
+
+#import "NetworkLib.h"
+
+@implementation NetworkLib
+
+@end
